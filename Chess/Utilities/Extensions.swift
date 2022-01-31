ad168638b435c08e20f54a4ddffd15399ece06c4
Aidan Lee <aidanwlee@gmail.com>
2022-01-31 14:21:54 -0500

    removing unneeded code

--- Chess/Utilities/Extensions.swift
+++ Chess/Utilities/Extensions.swift
@@ -8,23 +8,6 @@
 import Foundation
 import SwiftUI
 
-extension Binding {
-    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
-        Binding(
-            get: { self.wrappedValue },
-            set: { newValue in
-                self.wrappedValue = newValue
-                handler(newValue)
-            }
-        )
-    }
-}
-
-extension UIScreen {
-   static let screenWidth = UIScreen.main.bounds.size.width
-   static let screenHeight = UIScreen.main.bounds.size.height
-   static let screenSize = UIScreen.main.bounds.size
-}
 extension Int {
     func toLetterAtAlphabeticalIndex() -> Character {
         let startingValue = Int(("a" as UnicodeScalar).value)
@@ -57,33 +40,4 @@ extension Array where Element == (piece: Piece, count: Int) {
         }
     }
     
-}
-
-extension Array where Element == NSItemProvider {
-    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: NSItemProviderReading {
-        if let provider = first(where: { $0.canLoadObject(ofClass: theType)}) {
-            provider.loadObject(ofClass: theType) { object, error in
-                if let value = object as? T {
-                    DispatchQueue.main.async {
-                        load(value)
-                    }
-                }
-            }
-            return true
-        }
-        return false
-    }
-    func loadObjects<T>(ofType theType: T.Type, firstOnly: Bool = false, using load: @escaping (T) -> Void) -> Bool where T: _ObjectiveCBridgeable, T._ObjectiveCType: NSItemProviderReading {
-        if let provider = first(where: { $0.canLoadObject(ofClass: theType)}) {
-            let _ = provider.loadObject(ofClass: theType) { object, error in
-                if let value = object {
-                    DispatchQueue.main.async {
-                        load(value)
-                    }
-                }
-            }
-            return true
-        }
-        return false
-    }
 }
